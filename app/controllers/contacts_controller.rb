4283c402728a229220f9ca49a2391d6117756419
Kevin Hobbs <kevin21921@gmail.com>
2017-03-13 22:54:54 -0400

    Added contacts route.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,9 +1,18 @@
 class ContactsController < ApplicationController
-	
+
 	def new		
+		@contact = Contact.new
 	end
 
-	def create		
+	def create
+		@contact = Contact.new(contact_params)
 	end
-	
+
+
+	private
+
+		def contact_params
+			params.require(:contact).permit(:name, :email, :comments)
+		end
+
 end
