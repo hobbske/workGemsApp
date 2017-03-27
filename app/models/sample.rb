e18fdf645abd506119b134ecb2f974ef4221fb55
Kevin Hobbs <kevin21921@gmail.com>
2017-03-27 19:55:07 -0400

    Modified samples controller and views for responsive viewing.

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -1,7 +1,7 @@
 class Sample < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :img_before, styles: { carousel: "750x500#", large: "500x333#", medium: "300x200", small: "100x100#" }
+	has_attached_file :img_before, styles: { carousel: "750x500#", large: "600x333#", medium: "300x200", small: "100x100#" }
   validates_attachment_content_type :img_before, content_type: /\Aimage\/.*\Z/
 
   # has_attached_file :img_after, styles: { carousel: "750x500#", large: "500x333#", medium: "300x200#", small: "100x100#" }, default_url: "/images/:style/img_placeholder.png"
